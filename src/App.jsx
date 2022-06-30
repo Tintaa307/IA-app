ad32e69694065e52b0fd83d17bb6a985e9aa7c1e
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-06-29 23:07:56 -0300

    added brainjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import "./App.css"
 import Homepage from "./components/homepage/Homepage"
+import Calculate from "./components/calculatepage/Calculate"
 import { BrouserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <BrouserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/calculate" element={<Calculate />} />
         </Routes>
       </BrouserRouter>
     </>
---
+++ src/components/calculatepage/Calculate.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./calculate.css"
+
+const Calculate = () => {
+  return (
+    <div className="container-calculate">
+      <div className="container-content"></div>
+    </div>
+  )
+}
+
+export default Calculate
