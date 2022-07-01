4fd9009bf259bad5e33d74df61c4bbbc91cc6ca7
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-06-30 23:44:57 -0300

    finish console nav

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -13,14 +13,36 @@ const Homepage = () => {
         <div className="container-console">
           <div className="console">
             <div className="nav-console">
-              <div className="item">
-                <div className="rojo"></div>
+              <div className="container-items">
+                <div className="item">
+                  <div className="rojo"></div>
+                </div>
+                <div className="item">
+                  <div className="amarillo"></div>
+                </div>
+                <div className="item">
+                  <div className="verde"></div>
+                </div>
               </div>
-              <div className="item">
-                <div className="amarillo"></div>
-              </div>
-              <div className="item">
-                <div className="verde"></div>
+              <div className="container-ventanas">
+                <div className="ventana">
+                  <i class="ri-arrow-right-s-line"></i>
+                  <h6>Example</h6>
+                  <i class="ri-close-line"></i>
+                </div>
+                <div className="ventana">
+                  <i class="ri-arrow-right-s-line"></i>
+                  <h6>Brain</h6>
+                  <i class="ri-close-line"></i>
+                </div>
+                <div className="ventana">
+                  <i class="ri-arrow-right-s-line"></i>
+                  <h6>Train</h6>
+                  <i class="ri-close-line"></i>
+                </div>
+                <div className="container-more">
+                  <i class="ri-add-line"></i>
+                </div>
               </div>
             </div>
           </div>
